11ba4747b518b33a0babe3ab20bc576ca63876a5
Sundar <bssundarkumar@gmail.com>
2017-12-12 17:41:40 -0500

    components creation

--- angular2-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ angular2-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Recipe} from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] =[
+    new Recipe('Ckn Biryani','Curry desc',
+    'http://cdn-img.health.com/sites/default/files/styles/medium_16_9/public/1502736044/shrimp-tomato-avocado-salad-extra-tomato-recipes.jpg?itok=zs1txDL4'),
+    new Recipe('Avaikai Biryani','Avaikai desc',
+    'http://cdn-img.health.com/sites/default/files/styles/medium_16_9/public/1502736044/shrimp-tomato-avocado-salad-extra-tomato-recipes.jpg?itok=zs1txDL4')
+  
+  ];
   constructor() { }
 
   ngOnInit() {
